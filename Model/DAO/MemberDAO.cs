55f0501e881ac363df63d958b401875b70915751
duchoangse03239 <duchdse03239@fpt.edu.vn>
2016-10-21 14:49:57 -0700

    them ham changepass

--- Model/DAO/MemberDAO.cs
+++ Model/DAO/MemberDAO.cs
@@ -32,6 +32,16 @@ namespace Model.DAO
             return member.Count <= 0;
         }
 
+        public Member GetMemberByEmail(string email)
+        {
+            var dbmem = db.Members.Where(p => p.Email == email).ToList();
+            if (dbmem.Count>0)
+            {
+                     return dbmem.ElementAt(0);
+            }
+            return null;
+        }
+
         public bool AddMember(Member mem)
         {
             try { 
@@ -43,7 +53,22 @@ namespace Model.DAO
             {
                 return false;
             }
+        }
 
+        public bool CheckChangePass(string memberId,string password)
+        {
+            var member = db.Members.Find(int.Parse(memberId));
+            if (member.Password.Equals(password)) return false;
+            return true;
+        }
+
+        public int ChangePass(string memberId, string password)
+        {
+            var member = db.Members.Find(int.Parse(memberId));
+            member.Password = password;
+            db.Members.Attach(member);
+            db.Entry(member).Property(x => x.Password).IsModified = true;
+            return db.SaveChanges();
         }
     }
 }
