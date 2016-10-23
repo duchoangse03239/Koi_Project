0ba1fed461e9671339d324cc20724d29725ec874
duchoangse03239 <duchdse03239@fpt.edu.vn>
2016-10-23 00:31:21 -0700

    sua models

--- KoiManagement/Models/ActiveCode.cs
+++ KoiManagement/Models/ActiveCode.cs
@@ -19,7 +19,14 @@ namespace KoiManagement.Models
         public string ActCode { get; set; }
         public Nullable<System.DateTime> ExpireOn { get; set; }
         public bool Status { get; set; }
-    
+        public ActiveCode(){}
+        public ActiveCode(int MemberID, string ActCode, DateTime ExpireOn, bool Status)
+        {
+            this.MemberID = MemberID;
+            this.ActCode = ActCode;
+            this.ExpireOn = ExpireOn;
+            this.Status = Status;
+        }
         public virtual Member Member { get; set; }
     }
 }
