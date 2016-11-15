bc9ce76fd09fbc0402472ca95bc9285d14f1d953
thangncse02685 <thangncse02685@fpt.edu.vn>
2016-11-15 21:21:04 +0700

    lên

--- KoiManagement/Controllers/ArticleController.cs
+++ KoiManagement/Controllers/ArticleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KoiManagement.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,44 @@ namespace KoiManagement.Controllers
 {
     public class ArticleController : Controller
     {
-        // GET: Article
+        //
+        // GET: /Account/
         public ActionResult Index()
         {
             return View();
         }
+
+        //
+        // GET: /Account/Login
+        public ActionResult AddArticle(string returnUrl)
+        {
+            //if (Session[SessionAccount.SessionUserId] != null)
+            //{
+            //    return RedirectToAction("Index", "Home");
+            //}
+            //ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult AddArticle(string username, string password)
+        {
+            StatusObjForJsonResult obj = new StatusObjForJsonResult();
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                Common.Logger.LogException(ex);
+                obj.Status = 0;
+                obj.RedirectTo = this.Url.Action("SystemError", "Error");
+                return Json(obj);
+            }
+            return Json(obj);
+        }
+
     }
 }
