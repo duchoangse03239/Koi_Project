5237e719cde7e04e565dcb72f4cea2f4ab87df95
duongdxse03190 <duongdxse03190@gmail.com>
2016-11-17 09:03:15 +0700

    Article

---
+++ KoiManagement/DAL/ArticleDAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using KoiManagement.Models;
+
+namespace KoiManagement.DAL
+{
+    public class ArticleDAO
+    {
+        KoiManagementEntities db = null;
+         public ArticleDAO()
+        {
+            db = new KoiManagementEntities();
+        }
+        public bool AddArticle(Article Article)
+        {
+            try
+            {
+                db.Articles.Add(Article);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
